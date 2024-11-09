8e2c7ca69f24d20210f5f5f0c14f8a3c39ba587e
realcathode <ion.miron@tutanota.com>
2024-11-09 14:32:51 +0200

    Add key handling in main loop

--- src/handler.rs
+++ src/handler.rs
@@ -1,9 +1,11 @@
 use crate::app::{App, AppResult};
-use crossterm::event::KeyEvent;
-
+use crossterm::event::{self, Event, KeyEvent, KeyEventKind, KeyCode};
 /// Handles the key events and updates the state of [`App`].
 pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
     match key_event.code {
+        KeyCode::Char('q') => {
+            app.running = false;
+        }
         // TODO: define actions for quitting the app
         // TODO: define actions for apps functionalities
         _ => {}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use ratatui_templates::app::{App, AppResult};
 use ratatui_templates::event::{Event, EventHandler};
 use ratatui_templates::handler::handle_key_events;
 use ratatui_templates::tui::Tui;
+use crossterm::event::{self, KeyEvent, KeyEventKind, KeyCode};
 use std::io;
 use ratatui::backend::CrosstermBackend;
 use ratatui::Terminal;
@@ -9,26 +10,30 @@ use ratatui::Terminal;
 #[tokio::main]
 async fn main() -> AppResult<()> {
     // Create an application.
-    let app = App::new();
+    let mut app = App::new();
 
     // Initialize the terminal user interface.
     let backend = CrosstermBackend::new(io::stderr());
     let terminal = Terminal::new(backend)?;
 
-    let tick_rate = 200;
-    let events = EventHandler::new(tick_rate);
+    let events = EventHandler::new(200);
 
     let mut tui = Tui::new(terminal, events);
     
     // TODO: init the terminal
-    Tui::init(&mut tui);
+    Tui::init(&mut tui)?;
     // Start the main loop.
-    // while app.running {
+    while app.running {
         // TODO: Render the user interface. 
-        // TODO: Handle events.
-    // }
+        tui.draw(&mut app);
 
+        // TODO: Handle events.        
+        let evt = event::read()?;
+        if let crossterm::event::Event::Key(key) = evt {
+            handle_key_events(key, &mut app);            
+        }
+    }
     // TODO: Reset the terminal if the app has been terminated
-
+    tui.exit();
     Ok(())
 }
